98e3b9488b1267bd7851f583f6c98889ab9b3a79
vijaytembugade <vijaytembugade21@gmail.com>
2022-03-24 16:28:14 +0530

    Fix: Added auth reducer

--- src/Contexts/AuthContext/auth-context.js
+++ src/Contexts/AuthContext/auth-context.js
@@ -1,4 +1,5 @@
-const { createContext, useContext, useReducer, useEffect } = require("react");
+import { createContext, useContext, useReducer } from "react";
+import { authReducer } from "../../Reducers";
 
 const AuthContext = createContext();
 
@@ -9,70 +10,6 @@ function AuthProvider({ children }) {
     loading: false,
     error: null,
     isLoggedIn: localStorage.getItem("AUTH_TOKEN") ? true : false,
-  };
-
-  const authReducer = (state, action) => {
-    switch (action.type) {
-      case "LOGIN_REQUEST": {
-        return { ...state, loading: true };
-      }
-      case "LOGIN_SUCCESS": {
-        return {
-          ...state,
-          loading: false,
-          user: action.payload.user,
-          token: action.payload.token,
-          isLoggedIn: true,
-        };
-      }
-      case "LOGIN_ERROR": {
-        return {
-          ...state,
-          loading: false,
-          user: undefined,
-          token: undefined,
-          error: action.payload.erorrMessage,
-          isLoggedIn: false,
-        };
-      }
-
-      case "LOGOUT": {
-        return {
-          ...state,
-          loading: false,
-          user: undefined,
-          token: null,
-          error: undefined,
-          isLoggedIn: false,
-        };
-      }
-
-      case "SIGNUP_REQUEST": {
-        return { ...state, loading: true };
-      }
-
-      case "SIGNUP_SUCCESS": {
-        return {
-          ...state,
-          loading: false,
-          user: action.payload.user,
-          token: action.payload.token,
-          isLoggedIn: true,
-        };
-      }
-      case "SIGNUP_ERROR": {
-        return {
-          ...state,
-          loading: false,
-          user: undefined,
-          token: undefined,
-          error: action.payload.erorrMessage,
-          isLoggedIn: false,
-        };
-      }
-      default:
-        throw new Error(`Unhandled action type: ${action.type}`);
-    }
   };
 
   const [state, dispatch] = useReducer(authReducer, initialState);
