03d3508e6bd75ae51e60c0b11889f2e19d4f51a9
vijaytembugade <vijaytembugade21@gmail.com>
2022-03-27 16:34:37 +0530

    Fix: Route

--- src/Components/PrivateRoutes/PrivateRoutes.js
+++ src/Components/PrivateRoutes/PrivateRoutes.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../../Contexts";
 import Login from "../../Screens/Login/Login";
 
 const PrivateRoutes = ({ children }) => {
   const { state: authState } = useAuth();
-  return <>{authState.isLoggedIn ? children : <Login />}</>;
+  return (
+    <>{authState.isLoggedIn ? children : <Navigate replace to="/login" />}</>
+  );
 };
 
 export default PrivateRoutes;
